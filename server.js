5e6a45400f9fd57700ac4f824f6c8be622b591ab
Alexandre Castro <im-alexandre07@gmail.com>
2022-01-17 15:25:14 -0300

    -

--- server.js
+++ server.js
@@ -4,43 +4,45 @@ const jwt = require('fastify-jwt');
 fastify.register(jwt, {
   secret: 'key',
   cookie: { 
-    cookieName: 'token'
+    cookieName: 'token',
   }
 });
 
 fastify
   .register(require('fastify-cookie'));
 
+
+
 fastify.get('/cookies', async (request, reply) => {
-  const token = await reply.jwtSign({
-    name: 'foo',
-    role: ['admin', 'spy']
-  });
-      reply
-    .setCookie('token', token, {
-      domain: '',
-      path: '/',
-      //secure: true,
-      httpOnly: true,
-      sameSite: true
-    })
-    .code(200)
-    .send('Cookie sent')
+    const user = {
+        name: 'foo',
+        role: ['admin', 'spy']
+    };
+    const token = await reply.jwtSign(user, { expiresIn: "7d" });
+    reply.setCookie('token', token, {
+            domain: '',
+            path: '/',
+            //secure: true,
+            httpOnly: true,
+            sameSite: true
+        })
+        .code(200)
+        .send('Cookie sent')
 });
 fastify.get('/verifycookie', async (request, reply) => {
-  try {
-    await request.jwtVerify()
-    //reply.send({ code: 'OK', message: 'it works!' })
-    reply.send(request.user)
-  }
-  catch(error){
-    reply.send(error);
-  }
+    try {
+        await request.jwtVerify()
+        //reply.send({ code: 'OK', message: 'it works!' })
+        reply.send(request.user)
+    }
+    catch(error){
+        reply.send(error);
+    }
 });
 fastify.listen(3000, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  fastify.log.info(`server listening on ${address}`)
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    fastify.log.info(`server listening on ${address}`)
 })
